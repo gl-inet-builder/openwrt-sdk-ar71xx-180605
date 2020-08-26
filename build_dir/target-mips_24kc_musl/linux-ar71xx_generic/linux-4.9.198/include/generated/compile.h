55bcd1342cfaec8b6af6f6001927bba385c3c794
GL.iNet-Hongjian.Zhang <hongjian.zhang@gl-inet.com>
2020-08-26 17:14:07 +0800

    openwrt 18.06.5 sdk

---
+++ build_dir/target-mips_24kc_musl/linux-ar71xx_generic/linux-4.9.198/include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 0 */
+#define UTS_MACHINE "mips"
+#define UTS_VERSION "#0 Tue Nov 5 14:12:18 2019"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "lin"
+#define LINUX_COMPILER "gcc version 7.3.0 (OpenWrt GCC 7.3.0 r7897-9d401013fc) "
---
+++ staging_dir/toolchain-mips_24kc_gcc-7.3.0_musl/lib/gcc/mips-openwrt-linux-musl/7.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/data/patch_18.06.5/openwrt/build_dir/toolchain-mips_24kc_gcc-7.3.0_musl/gcc-7.3.0/configure --with-bugurl=http://www.lede-project.org/bugs/ --with-pkgversion='OpenWrt GCC 7.3.0 r7897-9d401013fc' --prefix=/data/patch_18.06.5/openwrt/staging_dir/toolchain-mips_24kc_gcc-7.3.0_musl --build=x86_64-pc-linux-gnu --host=x86_64-pc-linux-gnu --target=mips-openwrt-linux-musl --with-gnu-ld --enable-target-optspace --disable-libgomp --disable-libmudflap --disable-multilib --disable-libmpx --disable-nls --without-isl --without-cloog --with-host-libstdcxx=-lstdc++ --with-float=soft --with-gmp=/data/patch_18.06.5/openwrt/staging_dir/host --with-mpfr=/data/patch_18.06.5/openwrt/staging_dir/host --with-mpc=/data/patch_18.06.5/openwrt/staging_dir/host --disable-decimal-float --with-mips-plt --with-diagnostics-color=auto-if-env --disable-libssp --enable-__cxa_atexit --with-headers=/data/patch_18.06.5/openwrt/staging_dir/toolchain-mips_24kc_gcc-7.3.0_musl/include --disable-libsanitizer --enable-languages=c,c++ --enable-shared --enable-threads --with-slibdir=/data/patch_18.06.5/openwrt/staging_dir/toolchain-mips_24kc_gcc-7.3.0_musl/lib --enable-lto --with-libelf=/data/patch_18.06.5/openwrt/staging_dir/host";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "float", "soft" }, { "llsc", "llsc" }, { "mips-plt", "plt" } };
